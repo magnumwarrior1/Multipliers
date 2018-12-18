a1c6304ce56ee577bba2c0da721dec4325a5bd86
Yashas L.R <yash171019996@gmail.com>
2018-12-18 21:25:57 +0530

    Add files via upload

---
+++ vedic.v
@@ -0,0 +1,8 @@
+module vedic_2_x_2(a,b,c);
+input [1:0]a,b;
+output [3:0]c;
+assign c[0] =  a[0]&b[0];
+assign c[1] = (a[1]&b[0])^(a[0]&b[1]);
+assign c[2] = ((a[0]&b[1])&(a[1]&b[0]))^(a[1]&b[1]);
+assign c[3] = (a[1]&b[1])&((a[0]&b[1])&(a[1]&b[0]));
+endmodule
